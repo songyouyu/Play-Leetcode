51c849bbca975197a6dbbc4ceb3d5c9860c96182
songyouyu <songyy950208@gmail.com>
2020-03-27 10:26:18 +0800

    236

---
+++ 0236-Lowest-Common-Ancestor-of-a-Binary-Tree/src/Solution.java
@@ -0,0 +1,43 @@
+/**
+ * @author youyu.song
+ * @date 2020/3/27 10:15
+ */
+public class Solution {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+     *
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        } else if (left != null) {
+            return left;
+        } else {
+            return right;
+        }
+    }
+
+}
