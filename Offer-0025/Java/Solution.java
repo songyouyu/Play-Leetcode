920e06e393e45bc20897b01a3e6ecc76dcde110a
songyouyu <songyy950208@gmail.com>
2021-08-29 14:30:04 +0800

    解决了一些问题.

---
+++ Offer-0025/Java/Solution.java
@@ -0,0 +1,47 @@
+/**
+ * @author songyouyu
+ * @date 2021/8/29
+ */
+public class Solution {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummyNode = new ListNode(-1);
+        ListNode head1 = l1;
+        ListNode head2 = l2;
+        ListNode p = dummyNode;
+
+        while (head1 != null && head2 != null) {
+            if (head1.val < head2.val) {
+                p.next = head1;
+                head1 = head1.next;
+            } else {
+                p.next = head2;
+                head2 = head2.next;
+            }
+
+            p = p.next;
+        }
+
+        if (head1 != null) {
+            p.next = head1;
+        }
+        if (head2 != null) {
+            p.next = head2;
+        }
+
+        ListNode ret = dummyNode.next;
+        dummyNode.next = null;
+
+        return ret;
+    }
+
+}
